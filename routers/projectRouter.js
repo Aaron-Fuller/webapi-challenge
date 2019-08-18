a42c468682856d7f1da41939527a7b077bc566b2
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-08-18 14:04:11 -0400

    projects GET / done

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -3,6 +3,19 @@ const express = require('express');
 const db = require('../data/helpers/projectModel.js');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.get()
+        .then(projects => {
+            console.log(projects);
+            res.status(200).json(projects);
+        } )
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                err: 'error'
+            })
+        })
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = 'express';
 const server = require('express').Router();
-server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>server.js is working</h2>`)
